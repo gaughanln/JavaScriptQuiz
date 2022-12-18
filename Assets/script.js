52070e3ba34a33777f2697c8d038cb666a3dd25c
gaughanln <gaughanln@yahoo.com>
2022-12-17 18:25:30 -0600

    have semi functioning quiz but no timer

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,21 @@
 // GIVEN I am taking a code quiz
 var quizQuestion = document.querySelector(".questionHeader");
-var answers = document.querySelector(".answers");
-var timerEl = document.getElementById('countdown');
-var startQuizBtn = document.querySelector(".startQuiz");
+var answerChoices = document.querySelector(".answer-choices");
+var timerEl = document.getElementById("countdown");
+var startQuizBtn = document.querySelector(".start-quiz");
+var alertMsg = document.querySelector(".alert");
 
 var currentQuestionIndex = 0;
 var score = 0;
-
-
 
 // STEP 1:
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
 startQuizBtn.addEventListener("click", displayQuizQuestions);
 
-function countdown() {
-  var timeLeft = 80 
+// countdown timer (80 seconds)
+function startCountdown() {
+  var timeLeft = 80;
 }
 
 // STEP 2:
@@ -48,7 +48,7 @@ var choices = [
     question:
       "How do you write an IF statement for executing code if 'i' is NOT equal to 8?",
     answers: ["if (i != 8)", "if (i <= 8)", "if (i == 8)", "if (i === 8)"],
-    correctAnswer: "if (!=8)",
+    correctAnswer: "if (i != 8)",
   },
 
   {
@@ -59,75 +59,47 @@ var choices = [
 ];
 
 function displayQuizQuestions() {
+  // causes button to hide once the quiz begins
   startQuizBtn.classList.add("hidden");
+
+  if (currentQuestionIndex === choices.length) {
+    console.log("Quiz complete! Your score is: " + score);
+    return;
+  }
+
   var currentQuestion = choices[currentQuestionIndex];
-  console.log(currentQuestion.question)
-  console.log(currentQuestion.answers)
-  console.log(currentQuestion.correctAnswer)
-  quizQuestion.textContent= currentQuestion.question
+  console.log(currentQuestion.question);
+  console.log(currentQuestion.answers);
+  console.log(currentQuestion.correctAnswer);
+  quizQuestion.textContent = currentQuestion.question;
 
-  var currentAnswers = choices[currentQuestionIndex];
-  answers.textContent=currentAnswers.answers
-  
-// for loop for questions
+  answerChoices.innerHTML = "";
+  alertMsg.innerHTML = "";
+
+  // for loop for questions
+  var userAnswer = "";
   for (let index = 0; index < currentQuestion.answers.length; index++) {
-
-
-if (userAnswer === correctAnswer) { 
-    console.log("Correct answer chosen");
-    // text = "Correct!, Great job";
-  } else (userAnswer != correctAnswer); {
-    console.log("wrong answer chosen");
-    // text = "Try again.";
-    // needs to loop back but how
-  }
-  return;
-  }
-
-  // var userAnswer = "";
- 
-
-
-  // console.log(currentQuestion);
-
-  // countdown();
-}
-
-// currentAnswers.addEventListener("click", checkAnswers())
-
-// function checkAnswers() {
-//   if (userAnswer === correctAnswer) { 
-//     alert("Correct!, Great job");
-//     console.log();
    
-//   } else (userAnswer != correctAnswer); {
-//     alert("wrong answer chosen");
-//     console.log();
-//     // text = "Try again.";
-//     // needs to loop back but how
-// }
+    var answerItem = document.createElement("li");
+    answerItem.textContent = currentQuestion.answers[index];
+    answerItem.addEventListener("click", function () {
+      
+      if (this.textContent === currentQuestion.correctAnswer) {
+        currentQuestionIndex++;
+        displayQuizQuestions();
+        console.log("Correct answer chosen");
+      } else {
+        alertMsg.textContent = "❌ Wrong answer, try again. ❌"
+        console.log("❌ Wrong answer chosen ❌");
+      }
+    });
+    answerChoices.appendChild(answerItem);
+  }
+}
 
 // STEP 3:
 // WHEN I answer a question incorrectly
 // THEN time is subtracted from the clock
-
-// Function checkAnswers(){
-// for loop for answers - made to create them as answer buttons
-// for (let index = 0; index < currentAnswers.length; index++);
-// answers=[];
-//   // document.getElementById("text").innerHTML;
-//   // console.log(text);
-//   if (userAnswer === correctAnswer) { 
-//     console.log("Correct answer chosen");
-//     // text = "Correct!, Great job";
-//   } else (userAnswer != correctAnswer); {
-//     console.log("wrong answer chosen");
-//     // text = "Try again.";
-//     // needs to loop back but how
-//   }
-//   return;
-// }
-
 
 // STEP 4:
 // WHEN all questions are answered or the timer reaches 0
@@ -139,33 +111,14 @@ if (userAnswer === correctAnswer) {
 
 
 
-// function for answers/questions
-
-
-
-  // question.textContent= currentQuestion
-  // answers.textContent=currentAnswers
-
-
-  // look at past assignments for the timer situation. start by just having a timer in the first place. if it doesn't take time off - worry about that last.
-
-  
-  // must use concats to modify to make it look like i want?? list view
-
- 
-  // so we need/want a next question button to appear functional after the correct answer is given?
-
-
-
+// look at past assignments for the timer situation. start by just having a timer in the first place. if it doesn't take time off - worry about that last.
 
 
 // displayQuizQuestions();
 // countdown();
 // quiz choices + results (incorrect/try again or correct!)
 
-
-
 // what do we need?
 // // start timer when the quiz begins
-  // need a penalty for getting answer wrong
-  // make answers clickable
+// need a penalty for getting answer wrong
+// make answers clickable
